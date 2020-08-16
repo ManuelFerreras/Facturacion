8c0b6f4d736cfe9bf6673b177bbe2bfff8523d95
Manuel Ferreras <35044975+ManuelFerreras@users.noreply.github.com>
2020-08-16 13:23:38 -0300

    tabla

--- Facturaciones/src/facturaciones/Factura.java
+++ Facturaciones/src/facturaciones/Factura.java
@@ -92,7 +92,6 @@ public class Factura extends javax.swing.JDialog {
         setMaximumSize(new java.awt.Dimension(1280, 720));
         setMinimumSize(new java.awt.Dimension(1280, 720));
         setUndecorated(true);
-        setPreferredSize(new java.awt.Dimension(1280, 720));
         setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
